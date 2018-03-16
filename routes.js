a4ebecc3331c9eb01e5764e41f87cecf54dadca9
Deepak <deepakr63@gmail.com>
2018-03-16 19:29:27 +0530

    initial code

---
+++ routes.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Login from './pages/login';
+import SignUp from './pages/signup'
+import Home from './pages/home';
+import Profile from './pages/profile';
+import UpdateProfile from './pages/profileupdation';
+
+export default class App extends Component {
+	render() {
+		return(
+			<MuiThemeProvider>
+				<BrowserRouter>
+					<div>
+						<Route
+							exact
+							path="/"
+							component={ Login }
+						/>
+						 <Route
+                           
+							path="/signup"
+							component={ SignUp }
+						/>
+						<Route
+                           
+							path="/home"
+							component={ Home }
+						/>
+						 <Route 
+						 	exact
+                            path="/home/profile"
+                            component={ Profile }
+                        />
+						 <Route 
+                            path="/home/profileupdation"
+                            component={ UpdateProfile }
+                        />
+
+					</div>
+				</BrowserRouter>
+				</MuiThemeProvider>
+		)
+	}
+}
